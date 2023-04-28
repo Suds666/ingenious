7e88617d8abf89593dd69e32c3d760f0559b9e0c
Suds666 <joblesssuds@gmail.com>
2023-04-29 02:30:58 +0530

    second commit

--- farmforecast/farmapp/views.py
+++ farmforecast/farmapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+import urllib.request
+import json
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        source = urllib.request.urlopen()
+        season = request.POST['season']
